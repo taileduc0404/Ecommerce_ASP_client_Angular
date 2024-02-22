08a03b735830bcfd1183ed357e6f1d4c475887fc
taileduc0404 <taileduc0404@gmail.com>
2024-02-22 23:29:40 +0700

    Add Shared Module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpRequest } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+
+export class AppComponent implements OnInit{
   title = 'Ecommerce_ASP_client_Angular';
+  baseUrl="";
+  products : [] = [];
+
+
+  /**
+   *
+   */
+  constructor(private http:HttpClient) {
+    
+  }
+
+  getProduct(){
+    return this.http.get(this.baseUrl).subscribe(
+      (values:any)=>{
+        this.products=values;
+      }
+    )
+  }
+
+  ngOnInit(): void {
+    this.getProduct();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,9 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    SharedModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
