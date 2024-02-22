82566e2584dd26cac6d9afeb740c8515ebc0d393
taileduc0404 <taileduc0404@gmail.com>
2024-02-22 21:39:55 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Ecommerce_ASP_client_Angular';
+}
